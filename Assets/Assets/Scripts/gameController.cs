7011f0b637d0ba90f18ff3b949353ad444a17118
Murat Kacmaz <muratkacmaz@Murat-MacBook-Pro.local>
2018-10-08 14:38:41 +0300

    acilis sayfasi bitti

--- Assets/Assets/Scripts/gameController.cs
+++ Assets/Assets/Scripts/gameController.cs
@@ -1,29 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using System;
 
 public class gameController : MonoBehaviour {
 
-	public GameObject chicken , turtle ;
+	public GameObject chicken , turtle , nextButton,gameButton;
+	public Animator anim ;
+
+	public GameObject tavukKonusma, kaplumKonusma;
 	float speed =100;
+
 	void Start () {
-		 	
+		anim.enabled = false;
+		tavukKonusma.SetActive(false);
+		kaplumKonusma.SetActive(false);
+		nextButton.SetActive (false);
+		gameButton.SetActive (false);
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 
 	}
 
 	public void startClick (){
-		
-		Debug.Log(chicken.transform.rotation.eulerAngles.y);
+
+		//Debug.Log(chicken.transform.rotation.eulerAngles.y);
 		while (chicken.transform.rotation.eulerAngles.y >= 145) {
 			chicken.transform.Rotate (Vector3.down * Time.deltaTime * speed);
 			turtle.transform.Rotate (Vector3.up * Time.deltaTime * speed);
 		}
-		//turtle.transform.rotation.y = 45;
-	
+
+		anim.enabled = true;
+		tavukKonusma.SetActive(true);
+		nextButton.SetActive (true);
 	}
+
+	public void nextButtonClicked()
+	{
+		tavukKonusma.SetActive(false);
+		kaplumKonusma.SetActive(true);
+		gameButton.SetActive (true);
+		nextButton.SetActive (false);
+	}
+
+	public void gameButtonClicked () {
+		SceneManager.LoadScene ("infiniteRun");
+		
+	}
+
 }
