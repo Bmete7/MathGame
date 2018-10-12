8f0167700e4b3cbbdffa2f5c671ef8b0e838c734
Burak Mete <burak.mete11@gmail.com>
2018-10-12 22:18:47 +0300

    sahne bitis

--- Assets/Assets/Scripts/islemController.cs
+++ Assets/Assets/Scripts/islemController.cs
@@ -1,23 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class islemController : MonoBehaviour {
-	public int questionCount = 10;
-	public int anlatimCount = 4;
-	public GameObject[] sorular = new GameObject[10];
+
+
+	public GameObject soru;
 	public GameObject[] anlatim = new GameObject[4];
 	public GameObject[] cevapButonlari = new GameObject[4];
+
+	//anlatımlar arası gecis butonu
 	public GameObject nButton, bButton;
+	 
+	public string[] sorularA = new string[]{"3 + 5","4 - 2","6 + 3","2 x 1","8 / 2","4 x 2","7 - 2," ,"8 + 1", "8 - 1", "3 x 3"};
+	public string[] cevaplarA = new string[]{"8","2","9","2","4","8","5","9","7","9"};
+	public string[] cevaplarB = new string[]{"3","1","8","1","0","1","3","10","6","33"};
+	public string[] cevaplarC = new string[]{"2","3","7","21","1","7","10","8","8","1"};
+	public string[] cevaplarD = new string[]{"1","9","6","3","8","6","2","1","1","3"};
+	int[] soruSirasi = new int[5];
+
+
+
+	// kazanma kosulu icin
+	public int correctAnswers = 0;
+	public int totalAnswers = 0;
+
+	// tum konuları anlatınca soru butonu on yapar
+	private int anlatimIndex = 0;
 	public GameObject qStart;
 
-	private int anlatimIndex = 0;
 
+	// Button oryantasyonu icin kullanıldı
+	Vector3 tempA = new Vector3(-340.0f,0,0);
+	Vector3 tempB = new Vector3(0,0,0);
+	Vector3 tempC = new Vector3(-170.0f,0,0);
+	Vector3 tempD = new Vector3(170.0f,0,0);
+	Vector3 tempE = new Vector3(0,-50.0f,0);
 	// Use this for initialization
+
+
+
+
 	void Start () {
 		anlatim [anlatimIndex].SetActive (true);
 		qStart.SetActive (false);
-		sorular [0].SetActive (false);
+		soru.SetActive (false);
+		//sorular = 
 	}
 	
 	// Update is called once per frame
@@ -25,7 +54,7 @@ public class islemController : MonoBehaviour {
 		
 	}
 
-	public void nextInfo(){
+	void nextInfo(){
 		anlatim [anlatimIndex].SetActive (false);
 		if (anlatimIndex == 2) {
 			qStart.SetActive (true);
@@ -55,7 +84,7 @@ public class islemController : MonoBehaviour {
 		nButton.SetActive (false);
 		qStart.SetActive (false);
 		bool[] secilenSoruIndex= new bool[10];
-		int[] soruSirasi = new int[5];
+
 		for (int i = 0; i < 10; ++i) {
 			secilenSoruIndex [i] = false;
 		}
@@ -72,17 +101,69 @@ public class islemController : MonoBehaviour {
 			soruSirasi [i] = rand;
 		}
 
-		sorular [0].SetActive (true);
+		soru.SetActive (true);
+		for (int i = 0; i < 4; ++i) {
+			cevapButonlari[i].transform.position += tempE;
+		}
+		soru.transform.position += tempE;
+		//soru.transform.position += tempD;
+
+		buttonsMixed ();
 
 
-		Vector3 tempA = new Vector3(-300.0f,-50.0f,0);
-		Vector3 tempB = new Vector3(-100.0f,-50.0f,0);
-		Vector3 tempC = new Vector3(100.0f,-50.0f,0);
-		Vector3 tempD = new Vector3(300.0f,-50.0f,0);
+
+	}
+
+	public void correctAnswer(){
+		++correctAnswers;
+		anAnswer ();
+	}
+	public void falseAnswer(){
+		anAnswer ();
+	}
+	public void anAnswer(){
+		++totalAnswers;
+		if (totalAnswers == 5) {
+			if (correctAnswers >= 3) {
+				
+				Debug.Log ("Kazandin");
+			} 
+			else {
+				Debug.Log ("Kaybettin");
+			}
+		}
+
+		buttonsMixed ();
+	}
+
+	public void buttonsMixed(){
+		
+		soru.GetComponentInChildren<Text> ().text = sorularA[soruSirasi[totalAnswers]];
+		cevapButonlari [0].GetComponentInChildren<Text> ().text = cevaplarA[soruSirasi[totalAnswers]];
+		cevapButonlari [1].GetComponentInChildren<Text> ().text = cevaplarB[soruSirasi[totalAnswers]];
+		cevapButonlari [2].GetComponentInChildren<Text> ().text = cevaplarC[soruSirasi[totalAnswers]];
+		cevapButonlari [3].GetComponentInChildren<Text> ().text = cevaplarD[soruSirasi[totalAnswers]];
+
+
+			for (int i = 0; i < 4; ++i) {
+				Vector3 tempBos = new Vector3(cevapButonlari[i].transform.position[0] * -1 ,0,0); 
+				Vector3 tempBos2 = new Vector3(600.0f ,0,0); 
+				cevapButonlari [i].transform.position += tempBos;
+				cevapButonlari [i].transform.position += tempBos2;
+
+			}
+
+		
+		/*
+		for (int i = 0; i < 4; ++i) {
+			if(cevapButonlari[i].transform.position[0])
+		}*/
+ 
+
 
 		bool[] butonYeri = new bool[4];
-		breaker = 0;
-		rand = 0;
+		int breaker = 0;
+		int rand = 0;
 		for (int i = 0; i < 4; ++i) {
 			butonYeri [i] = false;
 		}
@@ -92,7 +173,6 @@ public class islemController : MonoBehaviour {
 				rand = Random.Range (0, 4);
 			} while(butonYeri [rand] && breaker < 200);
 			butonYeri[rand] = true;
-			Debug.Log (rand);
 			if (i == 0) {
 				cevapButonlari[rand].transform.position += tempA;	
 			}
@@ -109,8 +189,7 @@ public class islemController : MonoBehaviour {
 		for (int i = 0; i < 4; ++i) {
 			cevapButonlari [i].SetActive (true);
 		}
-		//cevapButonlari[0].GetComponentInChildren<Texture>().text
-	}
 
+	}
 
 }
